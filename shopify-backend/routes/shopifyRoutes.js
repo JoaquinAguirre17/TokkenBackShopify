e257018cb2f35a525618a6bbd3aa67215b342711
JoaquinAguirre17 <116034784+JoaquinAguirre17@users.noreply.github.com>
2025-02-10 20:15:27 -0300

    Delete shopify-backend directory

--- shopify-backend/routes/shopifyRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const shopifyController = require('../controllers/shopifyController');
-
-// Ruta para obtener productos
-router.get('/products', shopifyController.getProducts);
-
-module.exports = router;
