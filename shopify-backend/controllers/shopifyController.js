2ec27168e9c87144c352f496c00946f85130a2c2
Joaquin Aguirre <joaquinaguirre17@gmail.com>
2025-03-20 19:27:08 -0300

    intento 999

--- shopify-backend/controllers/shopifyController.js
+++ shopify-backend/controllers/shopifyController.js
@@ -1,41 +1,21 @@
 const axios = require('axios');
 
-// Función para obtener productos de Shopify con paginación
 const getProducts = async (req, res) => {
   try {
-    let products = [];
-    let hasNextPage = true;
-    let pageInfo = null;
-
-    // Bucle para manejar la paginación
-    while (hasNextPage) {
-      const response = await axios({
-        method: 'get',
-        url: `https://${process.env.SHOPIFY_STORE_URL}/admin/api/2025-01/products.json?limit=250&page_info=${pageInfo || ''}`,
-        headers: {
-          'X-Shopify-Access-Token': process.env.SHOPIFY_ACCESS_TOKEN,
-        },
-      });
-
-      // Concatenamos los productos obtenidos
-      products = products.concat(response.data.products);
-
-      // Extraemos el parámetro page_info para la siguiente página
-      pageInfo = response.headers['link']?.match(/&page_info=(\S+?)>/)?.[1];
-
-      // Si no hay más página, terminamos el bucle
-      hasNextPage = !!pageInfo;
-    }
-
-    res.status(200).json(products);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      message: 'Error al obtener productos',
-      error: error.response ? error.response.data : error.message,
+    const response = await axios({
+      method: 'get',
+      url: `https://${process.env.SHOPIFY_STORE_URL}/admin/api/2025-01/products.json?limit=250`,
+      headers: {
+        'X-Shopify-Access-Token': process.env.SHOPIFY_ACCESS_TOKEN,
+      },
     });
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error('Error al obtener productos:', error.response ? error.response.data : error.message);
+    res.status(500).json({ message: 'Error al obtener productos', error: error.response ? error.response.data : error.message });
   }
 };
+
 
 // Función para obtener los detalles de un producto específico
 const getProductDetails = async (req, res) => {
